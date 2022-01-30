20b618e92e81c3ab31c1a71c4c380ac0bc5c6557
Cuong Phan <vcuong0909@gmail.com>
2022-01-30 15:40:17 -0500

    init commit

---
+++ app/src/main/java/com/example/mealrecipe/view/HomePage.kt
@@ -0,0 +1,12 @@
+package com.example.mealrecipe.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mealrecipe.R
+
+class HomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_page)
+    }
+}
