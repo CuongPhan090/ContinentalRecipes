8608d01bee27dac42221b564f94407d41bdf9c14
Cuong Phan <vcuong0909@gmail.com>
2022-03-03 00:28:16 -0500

    added splash screen animation

--- app/src/main/java/com/example/mealrecipe/view/splashscreen/SplashScreen.kt
+++ app/src/main/java/com/example/mealrecipe/view/splashscreen/SplashScreen.kt
@@ -2,7 +2,12 @@ package com.example.mealrecipe.view.splashscreen
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.drawable.TransitionDrawable
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mealrecipe.BaseApplication
 import com.example.mealrecipe.databinding.ActivitySplashScreenBinding
@@ -11,26 +16,26 @@ import com.example.mealrecipe.view.category.CategoryView
 @SuppressLint("CustomSplashScreen")
 class SplashScreen : BaseApplication() {
     lateinit var binding: ActivitySplashScreenBinding
-//    lateinit var executorService: ScheduledExecutorService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val animation = AlphaAnimation(0f, 1f).apply {
+            duration = 5000
+        }
+        animation.setAnimationListener(object: Animation.AnimationListener{
+            override fun onAnimationStart(animation: Animation?) {}
+            override fun onAnimationRepeat(animation: Animation?) {}
+            override fun onAnimationEnd(animation: Animation?) {
+                binding.tapToContinue.visibility = View.VISIBLE
+            }
+        })
+        binding.logo.startAnimation(animation)
 
         binding.tapToContinue.setOnClickListener {
             startActivity(Intent(this, CategoryView::class.java))
         }
-//        executorService = Executors.newScheduledThreadPool(1)
-//        val runnable = Runnable {
-//            binding.tapToContinue.visibility = View.VISIBLE
-//            Toast.makeText(this, "Ran 3 seconds", Toast.LENGTH_LONG).show()
-//        }
-//        executorService.schedule(
-//            runnable,
-//            3,
-//            TimeUnit.SECONDS
-//        )
     }
 }
